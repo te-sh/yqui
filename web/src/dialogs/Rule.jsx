9839bfb1440828355d61e9a0cbfd5feec4a43401
te-sh <admire.japan@gmail.com>
2020-03-14 20:50:55 +0900

    add share button rule

--- web/src/dialogs/Rule.jsx
+++ web/src/dialogs/Rule.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import {
-  Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle,
-  FormControl, InputLabel, MenuItem, Select, TextField, Typography
+  Box, Button, Checkbox, Dialog, DialogActions, DialogContent,
+  DialogTitle, FormControl, FormControlLabel, InputLabel,
+  MenuItem, Select, Tabs, Tab, TextField, Typography
 } from '@material-ui/core'
 import { send } from '../communicate'
 import './Rule.scss'
 
+const TabPanel = ({ children, value, index, ...other }) => (
+  <Box className="tab-panel"
+       role="tabpanel"
+       hidden={value !== index}
+       id={`nav-tabpanel-${index}`}
+       aria-labelledby={`nav-tab-${index}`}
+       {...other}>
+    <Box>
+      {children}
+    </Box>
+  </Box>
+)
+
 const Rule = ({ open, close, ws, rule }) => {
+  const [tab, setTab] = React.useState(0)
   const [rightNum, setRightNum] = React.useState(0)
   const [pointCorrect, setPointCorrect] = React.useState(0)
   const [bonusCorrect, setBonusCorrect] = React.useState('none')
@@ -20,6 +35,7 @@ const Rule = ({ open, close, ws, rule }) => {
   const [losePointValue, setLosePointValue] = React.useState(0)
   const [loseBatsuActive, setLoseBatsuActive] = React.useState(true)
   const [loseBatsuValue, setLoseBatsuValue] = React.useState(0)
+  const [shareButton, setShareButton] = React.useState(false)
 
   const onEnter = () => {
     setRightNum(rule.rightNum)
@@ -34,6 +50,7 @@ const Rule = ({ open, close, ws, rule }) => {
     setLosePointValue(rule.losePoint.value)
     setLoseBatsuActive(rule.loseBatsu.active)
     setLoseBatsuValue(rule.loseBatsu.value)
+    setShareButton(rule.shareButton)
   }
 
   const onSubmit = evt => {
@@ -43,13 +60,14 @@ const Rule = ({ open, close, ws, rule }) => {
     send.rule(ws, {
       rightNum: parse(rightNum),
       pointCorrect: parse(pointCorrect),
-      bonusCorrect: bonusCorrect,
+      bonusCorrect,
       pointWrong: parse(pointWrong),
       batsuWrong: parse(batsuWrong),
       lockWrong: parse(lockWrong),
       winPoint: { active: winPointActive, value: parse(winPointValue) },
       losePoint: { active: losePointActive, value: parse(losePointValue) },
-      loseBatsu: { active: loseBatsuActive, value: parse(loseBatsuValue) }
+      loseBatsu: { active: loseBatsuActive, value: parse(loseBatsuValue) },
+      shareButton
     })
   }
 
@@ -64,68 +82,83 @@ const Rule = ({ open, close, ws, rule }) => {
       <form onSubmit={onSubmit}>
         <DialogTitle id="form-dialog-title">ルール</DialogTitle>
         <DialogContent className="rule">
-          <TextField label="解答権人数" type="number"
-                     value={rightNum}
-                     onChange={evt => setRightNum(evt.target.value)} />
-          <div>
-            <Typography>
-              正答時
-            </Typography>
-            <TextField label="ポイント" type="number"
-                       value={pointCorrect}
-                       onChange={evt => setPointCorrect(evt.target.value)} />
-            <FormControl>
-              <InputLabel id="bonus-correct-label">ボーナス</InputLabel>
-              <Select labelId="bonus-correct-label"
-                      value={bonusCorrect}
-                      onChange={evt => setBonusCorrect(evt.target.value)}>
-                <MenuItem value="none">なし</MenuItem>
-                <MenuItem value="cons">連答</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-          <div>
-            <Typography>
-              誤答時
-            </Typography>
-            <TextField label="ポイント" type="number"
-                       value={pointWrong}
-                       onChange={evt => setPointWrong(evt.target.value)} />
-            <TextField label="バツ" type="number"
-                       value={batsuWrong}
-                       onChange={evt => setBatsuWrong(evt.target.value)} />
-            <TextField label="休み" type="number"
-                       value={lockWrong}
-                       onChange={evt => setLockWrong(evt.target.value)} />
-          </div>
-          <div>
-            <Typography>
-              勝ち抜け
-            </Typography>
-            <Checkbox color="default" checked={winPointActive}
-                      onChange={evt => setWinPointActive(evt.target.checked)} />
-            <TextField label="ポイント" type="number"
-                       disabled={!winPointActive}
-                       value={winPointValue}
-                       onChange={evt => setWinPointValue(evt.target.value)} />
-          </div>
-          <div>
-            <Typography>
-              失格
-            </Typography>
-            <Checkbox color="default" checked={losePointActive}
-                      onChange={evt => setLosePointActive(evt.target.checked)} />
-            <TextField label="ポイント" type="number"
-                       disabled={!losePointActive}
-                       value={losePointValue}
-                       onChange={evt => setLosePointValue(evt.target.value)} />
-            <Checkbox color="default" checked={loseBatsuActive}
-                      onChange={evt => setLoseBatsuActive(evt.target.checked)} />
-            <TextField label="バツ" type="number"
-                       disabled={!loseBatsuActive}
-                       value={loseBatsuValue}
-                       onChange={evt => setLoseBatsuValue(evt.target.value)} />
-          </div>
+          <Tabs value={tab} onChange={(evt, newTab) => setTab(newTab)}>
+            <Tab label="通常" />
+            <Tab label="チーム" />
+          </Tabs>
+          <TabPanel value={tab} index={0}>
+            <TextField label="解答権人数" type="number"
+                       value={rightNum}
+                       onChange={evt => setRightNum(evt.target.value)} />
+            <div>
+              <Typography>
+                正答時
+              </Typography>
+              <TextField label="ポイント" type="number"
+                         value={pointCorrect}
+                         onChange={evt => setPointCorrect(evt.target.value)} />
+              <FormControl>
+                <InputLabel id="bonus-correct-label">ボーナス</InputLabel>
+                <Select labelId="bonus-correct-label"
+                        value={bonusCorrect}
+                        onChange={evt => setBonusCorrect(evt.target.value)}>
+                  <MenuItem value="none">なし</MenuItem>
+                  <MenuItem value="cons">連答</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div>
+              <Typography>
+                誤答時
+              </Typography>
+              <TextField label="ポイント" type="number"
+                         value={pointWrong}
+                         onChange={evt => setPointWrong(evt.target.value)} />
+              <TextField label="バツ" type="number"
+                         value={batsuWrong}
+                         onChange={evt => setBatsuWrong(evt.target.value)} />
+              <TextField label="休み" type="number"
+                         value={lockWrong}
+                         onChange={evt => setLockWrong(evt.target.value)} />
+            </div>
+            <div>
+              <Typography>
+                勝ち抜け
+              </Typography>
+              <Checkbox color="default" checked={winPointActive}
+                        onChange={evt => setWinPointActive(evt.target.checked)} />
+              <TextField label="ポイント" type="number"
+                         disabled={!winPointActive}
+                         value={winPointValue}
+                         onChange={evt => setWinPointValue(evt.target.value)} />
+            </div>
+            <div>
+              <Typography>
+                失格
+              </Typography>
+              <Checkbox color="default" checked={losePointActive}
+                        onChange={evt => setLosePointActive(evt.target.checked)} />
+              <TextField label="ポイント" type="number"
+                         disabled={!losePointActive}
+                         value={losePointValue}
+                         onChange={evt => setLosePointValue(evt.target.value)} />
+              <Checkbox color="default" checked={loseBatsuActive}
+                        onChange={evt => setLoseBatsuActive(evt.target.checked)} />
+              <TextField label="バツ" type="number"
+                         disabled={!loseBatsuActive}
+                         value={loseBatsuValue}
+                         onChange={evt => setLoseBatsuValue(evt.target.value)} />
+            </div>
+          </TabPanel>
+          <TabPanel value={tab} index={1}>
+            <FormControlLabel
+              control={
+                <Checkbox color="default"
+                          checked={shareButton}
+                          onChange={evt => setShareButton(evt.target.checked)} />
+              }
+              label="ボタン共有" />
+          </TabPanel>
         </DialogContent>
         <DialogActions>
           <Button type="submit" color="primary">
