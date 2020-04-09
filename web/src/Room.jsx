80a39d3394bfb2994c8c7968d8c230b4ffc0fba6
te-sh <admire.japan@gmail.com>
2020-04-09 23:00:05 +0900

    move websocket to Root

--- web/src/Room.jsx
+++ web/src/Room.jsx
@@ -1,57 +1,13 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import URI from 'urijs'
-import playSound from './sound'
-import {
-  reset, setWebSocket,
-  recvSelfID, recvRoom, recvUsers, recvTeams,
-  recvScores, recvButtons, recvRule, recvChat
-} from './redux/actions'
 import TopBar from './TopBar'
 import Chat from './Chat'
 import Messages from './Messages'
 import Mainarea from './Mainarea'
 import Subactions from './subactions/Subactions'
 import Actions from './actions/Actions'
-import EnterRoom from './dialogs/EnterRoom'
 import './Room.scss'
 
-const uri = URI(window.location.href).protocol('ws').pathname('/ws')
-
-const Room = ({ reset, setWebSocket, recv }) => {
-  const [enterRoomOpen, setEnterRoomOpen] = React.useState(true)
-
-  const enterRoom = name => {
-    setEnterRoomOpen(false)
-
-    const ws = new WebSocket(uri.query({ name }).toString())
-    setWebSocket(ws)
-
-    ws.onopen = evt => {
-      console.log('ws open', evt)
-    }
-
-    ws.onclose = evt => {
-      console.log('ws close', evt)
-      setEnterRoomOpen(true)
-      reset()
-    }
-
-    ws.onmessage = evt => {
-      console.log('ws received: ' + evt.data)
-      const data = JSON.parse(evt.data)
-      if (data.type === 'sound') {
-        playSound(data.content)
-      } else if (recv[data.type]) {
-        recv[data.type](data.content)
-      }
-    }
-
-    ws.onerror = evt => {
-      console.log('ws error: ' + evt.data)
-    }
-  }
-
+const Room = () => {
   return (
     <div className="room">
       <TopBar className="top-bar" />
@@ -60,25 +16,8 @@ const Room = ({ reset, setWebSocket, recv }) => {
       <Mainarea className="mainarea" />
       <Subactions className="subactions" />
       <Actions className="actions" />
-      <EnterRoom open={enterRoomOpen} submit={enterRoom} />
     </div>
   )
 }
 
-export default connect(
-  null,
-  dispatch => ({
-    reset: () => dispatch(reset()),
-    setWebSocket: ws => dispatch(setWebSocket(ws)),
-    recv: {
-      selfID: selfID => dispatch(recvSelfID(selfID)),
-      room: room => dispatch(recvRoom(room)),
-      users: users => dispatch(recvUsers(users)),
-      teams: teams => dispatch(recvTeams(teams)),
-      scores: scores => dispatch(recvScores(scores)),
-      buttons: buttons => dispatch(recvButtons(buttons)),
-      rule: rule => dispatch(recvRule(rule)),
-      chat: chat => dispatch(recvChat(chat))
-    }
-  })
-)(Room)
+export default Room
--- web/src/Root.jsx
+++ web/src/Root.jsx
@@ -1,13 +1,74 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import URI from 'urijs'
+import {
+  reset, setWebSocket,
+  recvSelfID, recvRoom, recvUsers, recvTeams,
+  recvScores, recvButtons, recvRule, recvChat
+} from './redux/actions'
+import playSound from './sound'
 import Room from './Room'
+import EnterRoom from './dialogs/EnterRoom'
 
-const Root = () => {
+const uri = URI(window.location.href).protocol('ws').pathname('/ws')
+
+const Root = ({ reset, setWebSocket, recv }) => {
+  const [enterRoomOpen, setEnterRoomOpen] = React.useState(true)
+
+  const enterRoom = name => {
+    setEnterRoomOpen(false)
+
+    const ws = new WebSocket(uri.query({ name }).toString())
+    setWebSocket(ws)
+
+    ws.onopen = evt => {
+      console.log('ws open', evt)
+    }
+
+    ws.onclose = evt => {
+      console.log('ws close', evt)
+      setEnterRoomOpen(true)
+      reset()
+    }
+
+    ws.onmessage = evt => {
+      console.log('ws received: ' + evt.data)
+      const data = JSON.parse(evt.data)
+      if (data.type === 'sound') {
+        playSound(data.content)
+      } else if (recv[data.type]) {
+        recv[data.type](data.content)
+      }
+    }
+
+    ws.onerror = evt => {
+      console.log('ws error: ' + evt.data)
+    }
+  }
+
   return (
     <Router>
       <Route exact path="/" component={Room}></Route>
+      <EnterRoom open={enterRoomOpen} submit={enterRoom} />
     </Router>
   )
 }
 
-export default Root
+export default connect(
+  null,
+  dispatch => ({
+    reset: () => dispatch(reset()),
+    setWebSocket: ws => dispatch(setWebSocket(ws)),
+    recv: {
+      selfID: selfID => dispatch(recvSelfID(selfID)),
+      room: room => dispatch(recvRoom(room)),
+      users: users => dispatch(recvUsers(users)),
+      teams: teams => dispatch(recvTeams(teams)),
+      scores: scores => dispatch(recvScores(scores)),
+      buttons: buttons => dispatch(recvButtons(buttons)),
+      rule: rule => dispatch(recvRule(rule)),
+      chat: chat => dispatch(recvChat(chat))
+    }
+  })
+)(Root)
