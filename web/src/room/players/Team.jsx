cf629ebcedcb416b214276858730a7a582a6f164
te-sh <admire.japan@gmail.com>
2020-07-27 18:12:03 +0900

    implement drag player to other team

--- web/src/room/players/Team.jsx
+++ web/src/room/players/Team.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
+import { useDrop } from 'react-dnd'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
 import { Box, Paper, Typography } from '@material-ui/core'
+import ItemTypes from '../../lib/item_types'
 import Players from './Players'
 import PlayerPoint from './PlayerPoint'
 import PlayerStatus from './PlayerStatus'
 import './Team.scss'
 
 const Team = ({ team, index, movePlayer, updateTeams, sg, rule }) => {
+  const [hover, setHover] = React.useState(false)
+
+  const [, dropRef] = useDrop({
+    accept: ItemTypes.PLAYER,
+    collect(monitor, _props) {
+      const item = monitor.getItem()
+      if (!item) {
+        return
+      }
+      const dragTeamIndex = item.teamIndex
+      if (dragTeamIndex === index) {
+        return
+      }
+      setHover(monitor.isOver())
+    },
+    drop(item, _monitor) {
+      if (index === item.teamIndex) {
+        return
+      }
+      droped(item, index)
+    }
+  })
+
+  const droped = (_item, _index) => {
+  }
+
   const teamScore = sg.team.scores.get(team.id)
-  const multiTeamClass = { 'multi-team': rule.team.active }
+  const teamClass = classNames('team', {
+    hover,
+    'multi-team': rule.team.active
+  })
 
   return (
-    <Box key={team.id} className={classNames('team', multiTeamClass)}>
+    <Box key={team.id} className={teamClass} ref={dropRef}>
       <Box className="team-point" hidden={!rule.team.active}>
         <Typography align="center">
           チーム得点
