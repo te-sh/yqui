5ff963da66cbec1a58a25b4bcfc9c39a1bb0706c
te-sh <admire.japan@gmail.com>
2020-11-17 20:55:33 +0900

    show comprehensive point

--- web/src/room/players/PlayerPoint.jsx
+++ web/src/room/players/PlayerPoint.jsx
@@ -5,21 +5,41 @@ import { Box, Typography } from '@material-ui/core'
 import './PlayerPoint.scss'
 
 const PlayerPoint = ({ className, user, score, rule }) => {
+  const comp = rule.player.comprehensive.active
+
   const pointText = user.isMaster || rule.showPoint ? score.point : '-'
   const batsuText = user.isMaster || rule.showPoint ? score.batsu : '-'
+  const compPointText = user.isMaster || rule.showPoint ? score.compPoint : '-'
+
+  const point = (
+    <Box className="point">
+      <Typography className="content">
+        {pointText}
+      </Typography>
+    </Box>
+  )
+
+  const batsu = (
+    <Box className="batsu">
+      <Typography className="content">
+        {batsuText}
+      </Typography>
+    </Box>
+  )
+
+  const compPoint = (
+    <Box className="comp-point">
+      <Typography className="content">
+        {compPointText}
+      </Typography>
+    </Box>
+  )
 
   return (
-    <Box className={classNames('player-point', className)}>
-      <Box className="correct">
-        <Typography className="content">
-          {pointText}
-        </Typography>
-      </Box>
-      <Box className="wrong">
-        <Typography className="content">
-          {batsuText}
-        </Typography>
-      </Box>
+    <Box className={classNames('player-point', comp && 'comp', className)}>
+      {point}
+      {comp && compPoint}
+      {batsu}
     </Box>
   )
 }
