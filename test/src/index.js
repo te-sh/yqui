51656faab8e18afa504fa13710e7d3956707d3c2
te-sh <admire.japan@gmail.com>
2021-02-08 18:43:50 +0900

    add simple test code with jest

--- test/src/index.js
+++ test/src/index.js
@@ -1,12 +1,28 @@
 const puppeteer = require('puppeteer');
 
-const YQUI_URL = 'http://ec2-13-115-155-138.ap-northeast-1.compute.amazonaws.com:8085/';
 const ROOT_DIR = '/home/node/test';
+const YQUI_URL = 'http://ec2-13-115-155-138.ap-northeast-1.compute.amazonaws.com:8085/';
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(YQUI_URL);
-  await page.screenshot({ path: `${ROOT_DIR}/screenshots/example.png` });
-  await browser.close();
-})();
+let browser;
+let page;
+
+describe('Test Yqui', () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ args: ['--disable-dev-shm-usage'] });
+    console.log(await browser.version());
+
+    page = await browser.newPage();
+    await page.goto(YQUI_URL);
+    console.log(await page.title());
+  }, 60000);
+
+  afterAll(async done => {
+    browser.close();
+    done();
+  });
+
+  test('show rooms page', async () => {
+    const title = await page.$eval('#root h6', el => el.textContent);
+    expect(title).toBe('Yqui');
+  });
+});
