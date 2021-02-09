6d61f4efd4601de5af228f48fde47f4c3ad191ea
te-sh <admire.japan@gmail.com>
2021-02-09 16:32:38 +0900

    add test of room users

--- test/src/index.js
+++ test/src/index.js
@@ -32,10 +32,46 @@ describe('rooms', () => {
     }
   });
 
-  test('show rooms page', async () => {
-    expect(await mpage.$eval('#root h6', el => el.textContent)).toBe('Yqui');
+  test('rooms page', async () => {
+    expect(await mpage.$eval('header .app-name', el => el.textContent)).toBe('Yqui');
     for (ppage of ppages) {
-      expect(await ppage.$eval('#root h6', el => el.textContent)).toBe('Yqui');
+      expect(await ppage.$eval('header .app-name', el => el.textContent)).toBe('Yqui');
     }
+  });
+
+  test('room users', async () => {
+    const firstRow = '.rooms-table tbody tr:first-child';
+
+    const enterRoom = async (page, index) => {
+      await page.click(`${firstRow} .enter-room-button button`);
+      await page.type('.enter-room .name input', `ゆーた${index}`);
+      await page.click('.enter-room-dialog .submit');
+      await page.waitForTimeout(100);
+    }
+
+    const leaveRoom = async page => {
+      await page.click('header .leave-room-button');
+      await page.click('.leave-room-dialog .submit');
+      await page.waitForTimeout(100);
+    }
+
+    const closePage = async page => {
+      await page.close();
+      await page.waitForTimeout(100);
+    }
+
+    expect(await mpage.$eval(`${firstRow} .num-users`, el => el.textContent)).toBe('0');
+
+    await enterRoom(ppages[0], 0);
+    expect(await mpage.$eval(`${firstRow} .num-users`, el => el.textContent)).toBe('1');
+
+    await enterRoom(ppages[1], 1);
+    expect(await mpage.$eval(`${firstRow} .num-users`, el => el.textContent)).toBe('2');
+
+    await leaveRoom(ppages[0]);
+    expect(await mpage.$eval(`${firstRow} .num-users`, el => el.textContent)).toBe('1');
+
+    await closePage(ppages[1]);
+    expect(await mpage.$eval(`${firstRow} .num-users`, el => el.textContent)).toBe('0');
   });
 });
