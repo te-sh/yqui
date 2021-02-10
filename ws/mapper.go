bd7488f6162bf83990c67b9c12d9f6a6223a1535
te-sh <admire.japan@gmail.com>
2021-02-10 09:52:52 +0900

    add mapper

---
+++ ws/mapper.go
@@ -0,0 +1,52 @@
+package main
+
+import "sync"
+
+type Mapper struct {
+	ID2Conn sync.Map
+	ID2Room sync.Map
+}
+
+func NewMapper() *Mapper {
+	mapper := new(Mapper)
+	return mapper
+}
+
+func (mapper *Mapper) RegisterConn(id int64, conn *Conn) {
+	mapper.ID2Conn.Store(id, conn)
+}
+
+func (mapper *Mapper) UnregisterConn(id int64) {
+	mapper.ID2Conn.Delete(id)
+}
+
+func (mapper *Mapper) GetConn(id int64) (conn *Conn, ok bool) {
+	_conn, _ok := mapper.ID2Conn.Load(id)
+	conn = _conn.(*Conn)
+	ok = _ok
+	return
+}
+
+func (mapper *Mapper) GetConns() []*Conn {
+	var conns []*Conn
+	mapper.ID2Conn.Range(func(_, value interface{}) bool {
+		conns = append(conns, value.(*Conn))
+		return true
+	})
+	return conns
+}
+
+func (mapper *Mapper) RegisterRoom(id int64, room *Room) {
+	mapper.ID2Room.Store(id, room)
+}
+
+func (mapper *Mapper) UnregisterRoom(id int64) {
+	mapper.ID2Room.Delete(id)
+}
+
+func (mapper *Mapper) GetRoom(id int64) (room *Room, ok bool) {
+	_room, _ok := mapper.ID2Room.Load(id)
+	room = _room.(*Room)
+	ok = _ok
+	return
+}
