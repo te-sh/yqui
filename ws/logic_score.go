db06ecda20c02b56b886ce6982213bb0936ef505
te-sh <admire.japan@gmail.com>
2021-02-11 11:53:14 +0900

    separate logic_win_lose.go

--- ws/logic_score.go
+++ ws/logic_score.go
@@ -8,25 +8,6 @@ func (ss *ScoreSet) CanPush(id int64) bool {
 	}
 }
 
-func (score *Score) ExceedWinPoint(rule WinLoseRule, comprehensive *ComprehensiveRule) bool {
-	if score.Win != 0 {
-		return false
-	}
-	if rule.WinPoint.Active {
-		if rule.WinPoint.Above && score.Point >= rule.WinPoint.Value ||
-			!rule.WinPoint.Above && score.Point <= rule.WinPoint.Value {
-			return true
-		}
-	}
-	if comprehensive != nil && comprehensive.Active && comprehensive.WinPoint.Active {
-		if comprehensive.WinPoint.Above && score.CompPoint >= comprehensive.WinPoint.Value ||
-			!comprehensive.WinPoint.Above && score.CompPoint <= comprehensive.WinPoint.Value {
-			return true
-		}
-	}
-	return false
-}
-
 func (ss *ScoreSet) SetCorrect(id int64, rule *Rule) {
 	if score, ok := ss.Scores[id]; ok {
 		score.Point += rule.Player.PointCorrect
@@ -56,47 +37,6 @@ func (ss *ScoreSet) SetCorrect(id int64, rule *Rule) {
 		}
 	}
 	ss.CalcCompPoint(rule.Player)
-}
-
-func (ss *ScoreSet) SetWin(rule WinLoseRule, comprehensive *ComprehensiveRule, passQuiz bool) (win bool) {
-	var wins []*Score
-	for _, score := range ss.Scores {
-		if score.ExceedWinPoint(rule, comprehensive) {
-			if !passQuiz || score.PassSeat {
-				wins = append(wins, score)
-				score.PassSeat = false
-			} else {
-				score.PassSeat = true
-			}
-		}
-	}
-	win = len(wins) > 0
-	if win {
-		ss.WinLose.WinNum += 1
-		for _, score := range wins {
-			score.Win = ss.WinLose.WinNum
-		}
-	}
-	return
-}
-
-func (score *Score) ExceedLosePoint(rule WinLoseRule) bool {
-	if score.Lose != 0 {
-		return false
-	}
-	if rule.LosePoint.Active {
-		if rule.LosePoint.Above && score.Point >= rule.LosePoint.Value ||
-			!rule.LosePoint.Above && score.Point <= rule.LosePoint.Value {
-			return true
-		}
-	}
-	if rule.LoseBatsu.Active {
-		if rule.LoseBatsu.Above && score.Batsu >= rule.LoseBatsu.Value ||
-			!rule.LoseBatsu.Above && score.Batsu <= rule.LoseBatsu.Value {
-			return true
-		}
-	}
-	return false
 }
 
 func (ss *ScoreSet) SetWrong(id int64, rule *Rule) {
@@ -137,23 +77,6 @@ func (ss *ScoreSet) SetWrong(id int64, rule *Rule) {
 	ss.CalcCompPoint(rule.Player)
 }
 
-func (ss *ScoreSet) SetLose(rule WinLoseRule) (lose bool) {
-	var loses []*Score
-	for _, score := range ss.Scores {
-		if score.ExceedLosePoint(rule) {
-			loses = append(loses, score)
-		}
-	}
-	lose = len(loses) > 0
-	if lose {
-		ss.WinLose.LoseNum += 1
-		for _, score := range loses {
-			score.Lose = ss.WinLose.LoseNum
-		}
-	}
-	return
-}
-
 func (ss *ScoreSet) Correct(id int64, rule *Rule, sound *Sound) {
 	ss.SetCorrect(id, rule)
 	sound.Win = ss.SetWin(rule.Player.WinLoseRule, rule.Player.Comprehensive, rule.Other.PassQuiz)
@@ -164,62 +87,6 @@ func (ss *ScoreSet) Wrong(id int64, rule *Rule, sound *Sound) {
 	ss.SetWrong(id, rule)
 	sound.Win = ss.SetWin(rule.Player.WinLoseRule, rule.Player.Comprehensive, rule.Other.PassQuiz)
 	sound.Lose = ss.SetLose(rule.Player.WinLoseRule)
-}
-
-func (ss *ScoreSet) WinTop(sound *Sound) {
-	alive := false
-	max := 0
-	for _, score := range ss.Scores {
-		if score.Win == 0 && score.Lose == 0 {
-			alive = true
-			max = score.Point
-		}
-	}
-	if !alive {
-		return
-	}
-	sound.Win = true
-	for _, score := range ss.Scores {
-		if score.Win == 0 && score.Lose == 0 {
-			if max < score.Point {
-				max = score.Point
-			}
-		}
-	}
-	ss.WinLose.WinNum += 1
-	for _, score := range ss.Scores {
-		if score.Win == 0 && score.Lose == 0 && score.Point == max {
-			score.Win = ss.WinLose.WinNum
-		}
-	}
-}
-
-func (ss *ScoreSet) LoseBottom(sound *Sound) {
-	alive := false
-	min := 0
-	for _, score := range ss.Scores {
-		if score.Win == 0 && score.Lose == 0 {
-			alive = true
-			min = score.Point
-		}
-	}
-	if !alive {
-		return
-	}
-	sound.Lose = true
-	for _, score := range ss.Scores {
-		if score.Win == 0 && score.Lose == 0 {
-			if min > score.Point {
-				min = score.Point
-			}
-		}
-	}
-	ss.WinLose.LoseNum += 1
-	for _, score := range ss.Scores {
-		if score.Win == 0 && score.Lose == 0 && score.Point == min {
-			score.Lose = ss.WinLose.LoseNum
-		}
-	}
 }
 
 func (ss *ScoreSet) DecreaseLock(buttons *Buttons) {
---
+++ ws/logic_win_lose.go
@@ -0,0 +1,132 @@
+package main
+
+func (score *Score) ExceedWinPoint(rule WinLoseRule, comprehensive *ComprehensiveRule) bool {
+	if score.Win != 0 {
+		return false
+	}
+	if rule.WinPoint.Active {
+		if rule.WinPoint.Above && score.Point >= rule.WinPoint.Value ||
+			!rule.WinPoint.Above && score.Point <= rule.WinPoint.Value {
+			return true
+		}
+	}
+	if comprehensive != nil && comprehensive.Active && comprehensive.WinPoint.Active {
+		if comprehensive.WinPoint.Above && score.CompPoint >= comprehensive.WinPoint.Value ||
+			!comprehensive.WinPoint.Above && score.CompPoint <= comprehensive.WinPoint.Value {
+			return true
+		}
+	}
+	return false
+}
+
+func (ss *ScoreSet) SetWin(rule WinLoseRule, comprehensive *ComprehensiveRule, passQuiz bool) (win bool) {
+	var wins []*Score
+	for _, score := range ss.Scores {
+		if score.ExceedWinPoint(rule, comprehensive) {
+			if !passQuiz || score.PassSeat {
+				wins = append(wins, score)
+				score.PassSeat = false
+			} else {
+				score.PassSeat = true
+			}
+		}
+	}
+	win = len(wins) > 0
+	if win {
+		ss.WinLose.WinNum += 1
+		for _, score := range wins {
+			score.Win = ss.WinLose.WinNum
+		}
+	}
+	return
+}
+
+func (score *Score) ExceedLosePoint(rule WinLoseRule) bool {
+	if score.Lose != 0 {
+		return false
+	}
+	if rule.LosePoint.Active {
+		if rule.LosePoint.Above && score.Point >= rule.LosePoint.Value ||
+			!rule.LosePoint.Above && score.Point <= rule.LosePoint.Value {
+			return true
+		}
+	}
+	if rule.LoseBatsu.Active {
+		if rule.LoseBatsu.Above && score.Batsu >= rule.LoseBatsu.Value ||
+			!rule.LoseBatsu.Above && score.Batsu <= rule.LoseBatsu.Value {
+			return true
+		}
+	}
+	return false
+}
+
+func (ss *ScoreSet) SetLose(rule WinLoseRule) (lose bool) {
+	var loses []*Score
+	for _, score := range ss.Scores {
+		if score.ExceedLosePoint(rule) {
+			loses = append(loses, score)
+		}
+	}
+	lose = len(loses) > 0
+	if lose {
+		ss.WinLose.LoseNum += 1
+		for _, score := range loses {
+			score.Lose = ss.WinLose.LoseNum
+		}
+	}
+	return
+}
+
+func (ss *ScoreSet) WinTop(sound *Sound) {
+	alive, max := false, 0
+	for _, score := range ss.Scores {
+		if score.Win == 0 && score.Lose == 0 {
+			alive = true
+			max = score.Point
+		}
+	}
+	if !alive {
+		return
+	}
+	sound.Win = true
+	for _, score := range ss.Scores {
+		if score.Win == 0 && score.Lose == 0 {
+			if max < score.Point {
+				max = score.Point
+			}
+		}
+	}
+	ss.WinLose.WinNum += 1
+	for _, score := range ss.Scores {
+		if score.Win == 0 && score.Lose == 0 && score.Point == max {
+			score.Win = ss.WinLose.WinNum
+		}
+	}
+}
+
+func (ss *ScoreSet) LoseBottom(sound *Sound) {
+	alive, min := false, 0
+	for _, score := range ss.Scores {
+		if score.Win == 0 && score.Lose == 0 {
+			alive = true
+			min = score.Point
+		}
+	}
+	if !alive {
+		return
+	}
+	sound.Lose = true
+	for _, score := range ss.Scores {
+		if score.Win == 0 && score.Lose == 0 {
+			if min > score.Point {
+				min = score.Point
+			}
+		}
+	}
+	ss.WinLose.LoseNum += 1
+	for _, score := range ss.Scores {
+		if score.Win == 0 && score.Lose == 0 && score.Point == min {
+			score.Lose = ss.WinLose.LoseNum
+		}
+	}
+}
