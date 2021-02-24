8314b7b2bd49e0c68b74edbcfbf7385e2bbeaf84
te-sh <admire.japan@gmail.com>
2021-02-25 08:33:46 +0900

    add join test (not success)

--- e2e-test/spec/support/yqui.rb
+++ e2e-test/spec/support/yqui.rb
@@ -10,6 +10,8 @@ module Yqui
     sleep WAIT_INTERVAL
   end
 
+  def topbar = find('.room .top-bar')
+
   def teams = all('.room .team')
   def team0 = teams[0]
 
@@ -22,10 +24,19 @@ module Yqui
 
   def last_message = find('.messages .message:last-child')
 
-  def enter_room(room: 1, name: nil)
+  def enter_room(room: 1, name: nil, observer: false, chat_answer: false)
     name ||= USER_NAME_PREFIX + /(\d+)\z/.match(Capybara.session_name.to_s).to_a[1]
     find(".rooms-table tbody tr:nth-child(#{room}) .enter-room-button button").click
+    wait
+
     find('.enter-room-dialog .enter-room .name input').set(name)
+
+    observer_check = find('.enter-room-dialog .enter-room .observer-check')
+    observer_check.click if observer != observer_check.has_css?('.Mui-checked')
+
+    chat_answer_check = find('.enter-room-dialog .enter-room .chat-answer-check')
+    chat_answer_check.click if chat_answer != chat_answer_check.has_css?('.Mui-checked')
+
     find('.enter-room-dialog .submit').click
     wait
   end
