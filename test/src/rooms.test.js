49fe3e730f098c67d5050f43720dc99a0e927a7c
te-sh <admire.japan@gmail.com>
2021-03-04 17:50:00 +0900

    remove test directory (jest + puppeteer)

--- test/src/rooms.test.js
+++
@@ -1,9 +0,0 @@
-describe('rooms', () => {
-  test('rooms page', async () => {
-    const s = 'header .app-name'
-
-    for (const page of browser.yq.pages) {
-      expect(await page.yq.textContent(s)).toBe('Yqui')
-    }
-  })
-})
